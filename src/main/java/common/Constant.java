963930ec7476fe3a32d3407a20b1a2bd2bfb2da2
tempdamac <darwincmacalindol@gmail.com>
2022-03-29 14:27:00 +0800

    Updated constant

--- src/main/java/common/Constant.java
+++ src/main/java/common/Constant.java
@@ -7,11 +7,6 @@ import java.util.Date;
 
 public class Constant {
 	
-	//Declare Constant Variable here
-	public final String ACT_AS_REQUIRED = "//*[contains(text(),'Act As is required')]";
-	public final String AVAILABILITY_DATE_REQUIRED = "//*[contains(text(),'Availability Date is required')]";
-	public final String EARLIEST_HIRE_DATE_REQUIRED = "//*[contains(text(),'Earliest Hire Date is required')]";
-	
 	private static final DateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy");
 
 	public String createDate(int dayAdded) {
